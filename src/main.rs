7595016162919186bedb8589969ce88d90dc5f24
Stefano Chiodino <not@going.2.happen>
2020-11-29 23:06:02 +0000

    use context

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@ use std::io::{stdout, stdin, Write, Read};
 use std::collections::HashMap;
 use std::collections::hash_map::RandomState;
 
+struct Context {
+    current_directory: String,
+    current_user: String,
+    args: String,
+}
+
 fn main() {
-    let mut tools: HashMap<&str, fn(String) -> String> = HashMap::new();
-    tools.insert("pwd", |x: String| format!("{}", x));
-    let user = "Stefano";
-    let current_path = "~";
-    let mut s= String::new();
-    loop{
-        print!("{}:{}$ " , user, current_path);
-        let _=stdout().flush();
+    let context = Context {
+        current_directory: "~".to_string(),
+        current_user: "Stefano".to_string(),
+        args: String::new(),
+    };
+    let mut tools: HashMap<&str, fn(&Context) -> String> = HashMap::new();
+    tools.insert("pwd", |c: &Context| format!("{}", c.current_directory));
+
+    let mut s = String::new();
+    loop {
+        print!("{}:{}$ ", context.current_user, context.current_directory);
+        let _ = stdout().flush();
         stdin().read_line(&mut s).expect("Did not enter a correct string");
         print!("{}", s);
         match tools.get(s.trim_end_matches("\n")) {
-            Some(x) => print!("{}", x("dsa".to_string())),
-            None=> print!("What?\n")
+            Some(x) => print!("{}", x(&context)),
+            None => print!("What?\n")
         }
     }
 }
