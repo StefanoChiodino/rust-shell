5454236ae6feab0ad5e5314b1d2615b0e43ecdb4
Stefano Chiodino <not@going.2.happen>
2020-11-29 23:38:03 +0000

    cd working

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{stdout, stdin, Write, Read};
 use std::collections::HashMap;
 use std::collections::hash_map::RandomState;
 use std::env;
+use regex::Regex;
 
 struct Context {
     current_directory: String,
@@ -9,14 +10,21 @@ struct Context {
     args: String,
 }
 
+fn cd(context: &mut Context) -> Option<String> {
+    let regex = Regex::new(r"[\\/][^\\/]+$").unwrap();
+    context.current_directory = regex.replace(&context.current_directory, "").to_string();
+    None
+}
+
 fn main() {
-    let context = Context {
+    let mut context = Context {
         current_directory: env::current_dir().unwrap().display().to_string(),
         current_user: "Stefano".to_string(),
         args: String::new(),
     };
-    let mut tools: HashMap<&str, fn(&Context) -> String> = HashMap::new();
-    tools.insert("pwd", |c: &Context| format!("{}", c.current_directory));
+    let mut tools: HashMap<&str, fn(&mut Context) -> Option<String>> = HashMap::new();
+    tools.insert("pwd", |c: &mut Context| Some(format!("{}", c.current_directory)));
+    tools.insert("cd", cd);
 
     loop {
         print!("{}:{}$ ", context.current_user, context.current_directory);
@@ -24,7 +32,11 @@ fn main() {
         let mut s = String::new();
         stdin().read_line(&mut s).expect("Did not enter a correct string");
         match tools.get(s.trim_end_matches("\n")) {
-            Some(x) => print!("{}\n", x(&context)),
+            Some(x) =>
+                match x(&mut context){
+                    Some(text) => print!("{}\n", text),
+                    _ => ()
+                },
             None => print!("What?\n")
         }
     }
